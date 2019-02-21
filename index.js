71f9d28d2b4b0ae36bed0671e5078a3cc48a6679
NolzCoding <34170158+NolzCoding@users.noreply.github.com>
2019-02-21 10:10:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function get() {
 	var password = document.getElementById('password')
 	var blob = new Blob(["pasword username:" + " " + name.value + " " + password], {type: "text/plain;charset=utf-8"});
 	var fs = require("fs");
-	window.alert("Username " + name.value " + " Password: " + password.value");
+	window.alert("Username " + name.value + " Password: " + password.value);
 	fs.writeFile("./password.txt", name.value + " " + password.value, (err) => {
 		if (err) {
 			console.log("err")
