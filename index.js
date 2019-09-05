11a8b19a0b184466002d0bb246d2f0c31a287717
NolzCoding <34170158+NolzCoding@users.noreply.github.com>
2019-09-05 10:26:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-function get() {
-	var name = document.getElementById('name')
-	var password = document.getElementById('password')
-	var blob = new Blob(["pasword username:" + " " + name.value + " " + password], {type: "text/plain;charset=utf-8"});
-	var fs = require("fs");
-	window.alert("Username " + name.value + " Password: " + password.value);
-	fs.writeFile("./password.txt", name.value + " " + password.value, (err) => {
-		if (err) {
-			console.log("err")
-			return;
-		}
-		console.log("Password and username has been collected")
-	}
-	saveAs(blob, "testfile1.txt");
-}
+
 
